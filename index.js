58e86f3f87a9b8e7ee9198cad71623d984326d59
Mark Lannen <mark.lannen@mso.umt.edu>
2022-07-21 08:47:53 -0600

    add package.json

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const fs = require('fs')
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+fs.readdir(process.cwd(), (err, filenames) => {
+    if(err) {
+console.log(err);
+    }
+    console.log(filenames);
+});
