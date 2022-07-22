ecc30db60b3cda8b3de522911d7f6b5e72117759
Mark Lannen <mark.lannen@mso.umt.edu>
2022-07-22 15:42:11 -0600

    finished project with 1 error outstanding. node:internal/errors:465     ErrorCaptureStackTrace(err);     ^
    TypeError [ERR_INVALID_ARG_VALUE]:

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
+import fs from 'fs';
+import util from 'util';
+import chalk from 'chalk';
+import path from 'path';
 
-fs.readdir(process.cwd(), (err, filenames) => {
+const { lstat} = fs.promises;
+
+const targetDir = process.argv[2] || process.cwd();
+
+fs.readdir(targetDir, process.cwd(), async (err, filenames) => {
     if(err) {
 console.log(err);
     }
-    console.log(filenames);
+
+    const statPromises = filenames.map(filename => {
+        return lstat(path.join(targetDir, filename));
+    });    
+
+    const allStats = await Promise.all(statPromises);
+
+    for (let stats of allStats) {
+        const index = allStats.indexOf(stats);
+
+        if (stats.isFile()) {
+            console.log(filenames[index]);
+        } else {
+            console.log(chalk.red(filenames[index], stats.isFile()))
+        }
+    }
 });
